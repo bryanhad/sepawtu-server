800f32ae2c6adb0d2f9e86b1c359a1af5b0dc334
bryanhad <bryanhadinata@gmail.com>
2023-10-31 15:18:28 +0700

    changing branch

--- client-user/src/components/ShoeCard.jsx
+++ client-user/src/components/ShoeCard.jsx
@@ -9,7 +9,6 @@ export default function ShoeCard({ shoe }) {
         >
             <div className="min-h-[300px] flex items-center bg-grayBackground">
                 <img src={shoe.mainImg} alt={`image of ${shoe.name}`} />
-
             </div>
             <div className="pb-6 flex-[1] flex flex-col gap-3">
                 <h4 className="flex-[1] text-sm">{shoe.name}</h4>
