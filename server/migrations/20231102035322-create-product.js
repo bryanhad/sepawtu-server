2c1b2d56aa35fa811fc6988030dd6611f4791134
bryanhad <bryanhadinata@gmail.com>
2023-11-02 11:03:18 +0700

    initialize sequelize

---
+++ server/migrations/20231102035322-create-product.js
@@ -0,0 +1,49 @@
+"use strict"
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Products", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            slug: {
+                type: Sequelize.STRING,
+            },
+            description: {
+                type: Sequelize.STRING,
+            },
+            price: {
+                type: Sequelize.INTEGER,
+            },
+            mainImg: {
+                type: Sequelize.STRING,
+            },
+            gender: {
+                type: Sequelize.STRING,
+            },
+            styleId: {
+                type: Sequelize.INTEGER,
+            },
+            authorId: {
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Products")
+    },
+}
