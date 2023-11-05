a4bbde2ec53fdcea9d1dea8bdc5e69f716698fd7
bryanhad <bryanhadinata@gmail.com>
2023-11-05 18:54:28 +0700

    trying to deploy server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
-const cors = require('cors')
-const express = require('express')
-const { routes } = require('./routes')
-const errorHandler = require('./middleware/errorHandler')
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+const cors = require("cors")
+const express = require("express")
+const { routes } = require("./routes")
+const errorHandler = require("./middleware/errorHandler")
 const app = express()
 const PORT = process.env.PORT || 3000
-
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
